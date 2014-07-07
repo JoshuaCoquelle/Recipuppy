e3a5717d3cea079dd1401fa8c2d613c7d7f95ba4
Joshua <brown@Joshua-Mac.local>
2014-07-06 18:26:35 -0700

    working comments on post w/o ajax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,22 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!
 
+  # def create
+  #   if Favorite.create(favorited: @project, user: current_user)
+  #     redirect_to @project, notice: 'Project has been favorited'
+  #   else
+  #     redirect_to @project, alert: 'Something went wrong...*sad panda*'
+  #   end
+  # end
+  
+  # def destroy
+  #   Favorite.where(favorited_id: @project.id, user_id: current_user.id).first.destroy
+  #   redirect_to @project, notice: 'Project is no longer in favorites'
+  # end
+  
+  # private
+  
+  # def set_project
+  #   @project = Project.find(params[:project_id] || params[:id])
+  # end
 end
