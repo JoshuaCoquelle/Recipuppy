21a10e8f70c28d18e44d5180caea04cc96d811b0
Joshua <brown@Joshua-Mac.local>
2014-07-06 17:26:58 -0700

    comment added to post show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,9 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
 
-    def create
+  def create
     @post = Post.find(params[:post_id])
-    @comment = Post.comments.new(params.require(:comment).permit([:body]))
+    @comment = Post.comments.new(params.require(:comment).permit(:body))
     @comment.user = current_user
     if @comment.save
       redirect_to post_path(@post), notice: "Comment Saved Successfully"
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,4 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!
+
 end
